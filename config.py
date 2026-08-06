379acbaad5cbc50c838e2d58fe2c97272552ee5b
footballusere-png <footballusere@gmail.com>
2026-08-06 10:02:29 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,27 @@
+# config.py
+
+API_ID = 39140696
+API_HASH = "64757b9724e7143c5cc554d7a776334b"
+BOT_TOKEN = "8832564199:AAHxLVgTOOS-leBSgT0fKVsrm8PpFP-BtlM"
+
+# ഡാറ്റാബേസ് ചാനൽ ഐഡി
+DB_CHANNEL = -1004296254082
+
+# ഫോഴ്സ് ജോയിൻ ചാനൽ ഐഡി
+FORCE_CHANNEL = -1003391211397
+
+# ഫയലുകൾ ഉള്ള പ്രൈവറ്റ് ചാനലുകൾ
+SOURCE_CHANNELS = [
+    -1003846249518,
+    -1002064631780,
+    -1003727771194,
+    -1002144792408,
+    -1004365921289,
+    -1004495469648
+]
+
+# MongoDB URI
+MONGO_URI = "mongodb+srv://footballusere_db_user:risham004@cluster0.k2t3crf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# അഡ്മിൻമാരുടെ ടെലഗ്രാം യൂസർ ഐഡി ഇവിടെ നൽകുക
+ADMINS = [123456789] 
