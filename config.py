c39fd30796493b50f48291dbc90d0534a950780e
footballusere-png <footballusere@gmail.com>
2026-08-06 10:26:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ API_ID = 39140696
 API_HASH = "64757b9724e7143c5cc554d7a776334b"
 BOT_TOKEN = "8832564199:AAHxLVgTOOS-leBSgT0fKVsrm8PpFP-BtlM"
 
+# ടെലഗ്രാം യൂസർബോട്ടിനായി ജനേറ്റ് ചെയ്ത സ്ട്രിംഗ് താഴെ നൽകുക
+STRING_SESSION = "1BVtsOKwBu6I7iMU1D0fFYvKrPlJ6CBykqVDShfYeGFtflC7jEvU7sfk3t1URVll7Xl2foYv98JxPeA2Cb8_8d1SXAhn9XrKaa8XyNv8efcfSvLi9vdM0nn8agZU57WV4o9j0peY2iGhhOK7hnnadZSCD07Wvwq-Uwye1AOn_PYw0sVawEJOelvFiUBd5xwopErBj2ycqLWtW1o3vQCTET5s7-hIu-kYDc6GNwUqe7IMgvsOG6rdpqj1DUxR60h2J3m5XHQodwA0rQ2yeqKgV-j7Gn5adzAPxhNI76lJTcDvmHCRIqeoQnvodthnrXmj31UW0qgEWytm-5wGvVBsxOR-NA1PsRyU="
+
 # ഡാറ്റാബേസ് ചാനൽ ഐഡി
 DB_CHANNEL = -1004296254082
 
@@ -23,5 +26,5 @@ SOURCE_CHANNELS = [
 # MongoDB URI
 MONGO_URI = "mongodb+srv://footballusere_db_user:risham004@cluster0.k2t3crf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-# അഡ്മിൻമാരുടെ ടെലഗ്രാം യൂസർ ഐഡി ഇവിടെ നൽകുക
+# അഡ്മിൻമാരുടെ ടെലഗ്രാം യൂസർ ഐഡി
 ADMINS = [123456789] 
