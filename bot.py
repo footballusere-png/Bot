45743e38b04a0dc84ff5521f84d6fa577cf7f410
footballusere-png <footballusere@gmail.com>
2026-08-08 19:17:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,31 @@
 from datetime import datetime
+import json
 import firebase_admin
 from firebase_admin import credentials, db
 import telebot
 
-# 1. നിങ്ങൾ നൽകിയ വെബ് കോൺഫിഗറേഷനിൽ നിന്നുള്ള ഫയർബേസ് വിവരങ്ങൾ
-# (പൈത്തൺ ബാക്ക്എൻഡിനായി ഇത് സർവീസ് അക്കൗണ്ട് ഫോർമാറ്റിലേക്ക് ക്രമീകരിച്ചിരിക്കുന്നു)
+# 1. നിങ്ങൾ നൽകിയ ഒറിജിനൽ ഫയർബേസ് സർവീസ് അക്കൗണ്ട് ജെയ്‌സൺ കീ വിവരങ്ങൾ
 firebase_key_data = {
     "type": "service_account",
     "project_id": "a-one-chat-e3642",
-    "private_key_id": "a1b2c3d4e5f67890",  # ഓട്ടോമാറ്റിക് ആയി സെറ്റ് ചെയ്തത്
+    "private_key_id": "0b38269e5fc5186cf81ee268a40470eca861e9d5",
     "private_key": (
-        "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC3qNq...\n-----END"
-        " PRIVATE KEY-----\n"
+        "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDZa5uFfJabdEOz\nRI9iwts9++tx7pUBIXIK5loiGBVTJtDyUJTVBvyd4wgjZW66JrUtxnW1QQ35fMSG\n9nOxBFJHHgEPWIoEKMrnqq+USJJ+IGIbA3iRqpwzxgly5jk8GvwIs1aiVDoDxsQ/\nvKQpJysPVq8BCXdVhKu9hXyLfpjQ56MjXIsyJC+kwD8MWJ1pV7UIYEtCNRGQDlyt\nP1aLBoEyCmTANO2ZlZQosfTDiaMkg3iLBxaiZyxzHIYJ+r8FJUwFsXPbL1+GQBgd\nZ+bG7LuMxgda9YeD5nagaaHzuBiVybjLrfKFXmK4q5n+nPDVGAiTVkaJPb8PHN7H\nCeALfuvBAgMBAAECggEABDZhREASc+8X+7Jtm8J+OHB30pSGhwfUm5D0eJoiWaLU\nJAm7JHh27wEt0G+/6jPjSUCB2dHpmSaZcSr3uVuF05Jpf1heDjATJFNqhPM2AWMT\nu4XA8YOOdLNWzgbXX4p/TRTabhk3KYkRltGLFbAIVcr+Z4T9Lqc8I5chP1ujztJQ\n7GK6PLVuxv14aaB+DAFNJQ2rTV2/d6mt2zKaazSjhEHcXG6u1PdmqMAjZ5dDbFDD\nvxrpp9nQmE5YSmfWxNMrElz8bAhlaQv4xnkXpXhZQIa27+6FHT6a2slUb34ICIZ4\nJZ6gEnGBlfUqKXFS4hxatPAlAgwflqDvAE+gjjLU9QKBgQD9+/B9mfOXLw21Dadf\n3pSNNoXhctiQoW6brdFhJ7Vd0PIpyzRIMbjrdfSmyIUXy28T1si4sEbwoH50ebaq\nTFFASx7OrhF1MzDcl4x2o5nYRnKskZues7xaVKDuLKUCUOWYa4O7N1KwjIgBDWHi\n1TcQHDftusV+ZciE55WK5o4VXQKBgQDbJWAiEjuv6boeg8bt56Lq3FMCBZXlgV6t\n0Z7taY4uvbX4Q6x4HPcyRQBPZWLL2UTEsuapmVIYAwReVAHqjTECzQ8veuA7liul\nR+MtFqxpBU/IiT8V3v1IHmIv21fu5Hfs8i4CEJaAbmty6pSOZAiGF0fj7qayhddG\nA9HonCsFtQKBgHm7LgnVJuY4PDah463UbZi9IC/tLpUremsNRURulsPvaJHVfip4\nAmyAbZEenIPKEmQM1smGW4nrMpC64W0ABRVuq8ZdsapdrbacwEsAoLUDFuVVKKRI\n+ybEVxmwtinFAjYqmcm6e1x5DpqKgncIwEpta/T6RrwNJq1knc0kMcdVAoGBAJ67\njcxgBJfA5i0gKkE8XQddG0sFnLOmZ1vj0AgLQw+cHmh7LDu8T1k8HaNkvpEFCQio\nxObnxUzbMpjYpKKuLmfm/C7aA/1lIqwPS//mwm83h7iroORIppFMYLZlXGYPWsZo\ndOc1+K2CTZXpUD1rO7lUt5/jxx7cTzfJoIuqQodZAoGBAJugUTuJ9p/TCgjkWbwS\nGyl37wGjodNWMyoqWmza5MUJv0zSOEtUGCdNES4tcYGJtXezaQG2useh1Oc2RS5P\nyIq+/a7y9Ok1X/LeaPivdvbuyjgeXiLPwomsBYDk+2h2H7SXxuGdh+6djF+wCWYx\nhed1kP2euE6OTAYw4TyKS8jv\n-----END PRIVATE KEY-----\n"
     ),
     "client_email": (
         "firebase-adminsdk-fbsvc@a-one-chat-e3642.iam.gserviceaccount.com"
     ),
-    "client_id": "91366490582",
+    "client_id": "116800017912460308477",
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
     "client_x509_cert_url": (
         "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40a-one-chat-e3642.iam.gserviceaccount.com"
     ),
+    "universe_domain": "googleapis.com",
 }
 
-# ഫയർബേസ് ഇനിഷ്യലൈസ് ചെയ്യുന്നു
+# ഫയർബേസ് അഡ്മിൻ എസ്.ഡി.കെ ഇനിഷ്യലൈസ് ചെയ്യുന്നു
 cred = credentials.Certificate(firebase_key_data)
 firebase_admin.initialize_app(
     cred,
